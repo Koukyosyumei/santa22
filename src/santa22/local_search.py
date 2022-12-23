b3bf58cc688504697bc527b752325e0031e6f171
Koukyosyumei <koukyosyumei@hotmail.com>
2022-12-23 10:49:54 +0900

    update

--- src/santa22/local_search.py
+++ src/santa22/local_search.py
@@ -65,6 +65,74 @@ def calc_threshold(improve, t_start, t_final, current_itr, max_itr):
 
 
 @njit
+def three_opt(config, image_lut):
+    offset = 1  # TODO support larger offset
+
+    i = random.randint(1, len(config) - (3 + 2 * offset))
+    j = i + offset + 1
+    k = j + offset + 1
+
+    d_AB = evaluate_config(config[i - 1 : i + 1], image_lut)
+    d_CD = evaluate_config(config[j - 1 : j + 1], image_lut)
+    d_EF = evaluate_config(config[k - 1 : k + 1], image_lut)
+
+    p_AC = get_path_to_configuration(config[i - 1], config[j - 1])
+    p_BD = get_path_to_configuration(config[i], config[j])
+    p_CE = get_path_to_configuration(config[j - 1], config[k - 1])
+    p_DF = get_path_to_configuration(config[j], config[k])
+    p_AD = get_path_to_configuration(config[i - 1], config[j])
+    p_EB = get_path_to_configuration(config[k - 1], config[i])
+    p_CF = get_path_to_configuration(config[j - 1], config[k])
+    p_FB = get_path_to_configuration(config[k], config[i])
+    p_EA = get_path_to_configuration(config[k - 1], config[i - 1])
+
+    d_AC = evaluate_config(p_AC, image_lut)
+    d_BD = evaluate_config(p_BD, image_lut)
+    d_CE = evaluate_config(p_CE, image_lut)
+    d_DF = evaluate_config(p_DF, image_lut)
+    d_AD = evaluate_config(p_AD, image_lut)
+    d_EB = evaluate_config(p_EB, image_lut)
+    d_CF = evaluate_config(p_CF, image_lut)
+    d_FB = evaluate_config(p_FB, image_lut)
+    d_EA = evaluate_config(p_EA, image_lut)
+
+    d0 = d_AB + d_CD + d_EF
+    d1 = d_AC + d_BD + d_EF
+    d2 = d_AB + d_CE + d_DF
+    d3 = d_AD + d_EB + d_CF
+    d4 = d_FB + d_CD + d_EA
+
+    if d0 > d1:
+        # ABCD -> ACBD
+        return np.concatenate((config[: i - 1], p_AC, p_BD, config[j + 1 :])), -d0 + d1
+    elif d0 > d2:
+        # CDEF -> CEDF
+        return np.concatenate((config[: j - 1], p_CE, p_DF, config[k + 1 :])), -d0 + d2
+    elif d0 > d4:
+        # ABCDEF -> AEDCBF
+        return (
+            np.concatenate(
+                (
+                    config[: i - 1],
+                    p_EA[::-1],
+                    config[j - 1 : j + 1][::-1],
+                    p_FB[::-1],
+                    config[k + 1 :],
+                )
+            ),
+            -d0 + d4,
+        )
+    elif d0 > d3:
+        # ABCDEF -> ADEBCF
+        return (
+            np.concatenate((config[: i - 1], p_AD, p_EB, p_CF, config[k + 1 :])),
+            -d0 + d3,
+        )
+
+    return config, 0
+
+
+@njit
 def two_opt(config, offset, image_lut):
     i = random.randint(0, len(config) - (3 + offset))
 
@@ -100,7 +168,7 @@ def local_search(config, image_lut, max_itr=10, t_start=0.3, t_end=0.01):
         else:
             offset = random.choices(offset_choice, weights=offset_choice_weight_far)[0]
 
-        config_new, improve = two_opt(config, offset, image_lut)
+        config_new, improve = three_opt(config, image_lut)
 
         if improve < 0 or random.random() < calc_threshold(
             improve * -1, t_start, t_end, itr, max_itr
@@ -108,8 +176,8 @@ def local_search(config, image_lut, max_itr=10, t_start=0.3, t_end=0.01):
             best_score = best_score + improve
             config = config_new
 
-        if itr % 10000 == 0:
-            config = run_remove(config)
+        if itr % 100000 == 0:
+            # config = run_remove(config)
             print(best_score)
 
     config = run_remove(config)
