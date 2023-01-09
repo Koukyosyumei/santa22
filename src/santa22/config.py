8c9534b5bed7453e08b5fe5c4cdbae4649ca56c7
Koukyosyumei <koukyosyumei@hotmail.com>
2023-01-09 20:08:56 +0900

    update baseline

--- src/santa22/config.py
+++ src/santa22/config.py
@@ -5,33 +5,22 @@ import numpy as np
 from numba import njit
 from tqdm import tqdm
 
-from .utils import get_origin, get_path_to_configuration, get_position, points_to_path
+from .utils import (
+    get_origin,
+    get_path_to_configuration,
+    get_position,
+    points_to_path,
+    run_remove,
+)
 
 
 def standard_config_topright(x, y):
-    """Return the preferred configuration (list of eight pairs) for the point (x,y)"""
+    """Return the preferred configuration (list of eight pairs) for the point (x,y)
+    [(64, _), (_, 32), (_, 16), (_, 8), (_, 4), (_, 2), (_, 1), (_, 1)]
+    """
     assert x > 0 and y >= 0, "This function is only for the upper right quadrant"
     r = 64
     config = [(r, y - r)]  # longest arm points to the right
-    x = x - config[0][0]
-    while r > 1:
-        r = r // 2
-        arm_x = np.clip(x, -r, r)
-        config.append((arm_x, r))  # arm points upwards
-        x -= arm_x
-    arm_x = np.clip(x, -r, r)
-    config.append((arm_x, r))  # arm points upwards
-    assert x == arm_x
-    return config
-
-
-def standard_config_topleft(x, y):
-    """Return the preferred configuration (list of eight pairs) for the point (x,y)"""
-    assert x <= 0 and y > 0, "This function is only for the upper right quadrant"
-    r = 64
-    x2 = x - r
-    x2 = np.clip(x2, -r, r)
-    config = [(x2, y - r)]  # longest arm points to the right
     x = x - config[0][0]
     while r > 1:
         r = r // 2
@@ -61,29 +50,39 @@ def standard_config_bottomright(x, y):
     return config
 
 
-def standard_config_bottomleft(x, y):
+def standard_config_topleft(x, y):
     """Return the preferred configuration (list of eight pairs) for the point (x,y)"""
-    assert (
-        x < 0 and y <= 0
-    ), f"This function is only for the upper right quadrant; {(x, y)}"
+    assert x <= 0 and y > 0, "This function is only for the upper left quadrant"
+    # (_, 64), (-32, _), (-16, _) ,(-8, _), (-4, _), (-2, _), (-1, _), (-1, _)
     r = 64
-    x2 = x - r
-    x2 = np.clip(x2, -r, r)
-    y2 = y - r
-    y2 = np.clip(y2, -r, r)
-    config = [(x2, y2)]  # longest arm points to the right
+    config = [(x - (-r), r)]  # longest arm points to the top
     y = y - config[0][1]
-    x = x - config[0][0]
     while r > 1:
         r = r // 2
         arm_y = np.clip(y, -r, r)
-        arm_x = np.clip(x, -r, r)
-        config.append((arm_x, arm_y))  # arm points upwards
+        config.append((-r, arm_y))  # arm points leftwards
         y -= arm_y
-        x -= arm_x
     arm_y = np.clip(y, -r, r)
+    config.append((-r, arm_y))  # arm points leftwards
+    assert y == arm_y
+    return config
+
+
+def standard_config_bottomleft(x, y):
+    """Return the preferred configuration (list of eight pairs) for the point (x,y)"""
+    assert x < 0 and y <= 0, "This function is only for the lower left quadrant"
+    # (-64, _),(_, -32), (_, -16) ,(_, -8), (_, -4), (_, -2), (_, -1), (_, -1)
+    r = 64
+    config = [(-r, y - (-r))]  # longest arm points to the left
+    x = x - config[0][0]
+    while r > 1:
+        r = r // 2
+        arm_x = np.clip(x, -r, r)
+        config.append((arm_x, -r))  # arm points downwards
+        x -= arm_x
     arm_x = np.clip(x, -r, r)
-    config.append((arm_x, arm_y))  # arm points upwards
+    config.append((arm_x, -r))  # arm points downwards
+    assert x == arm_x
     return config
 
 
@@ -101,7 +100,6 @@ def standard_config(x, y):
 
 
 def get_baseline():
-    # Generate points
     points_baseline = []
     flag = True
     for split in range(2):
@@ -117,6 +115,170 @@ def get_baseline():
     points_baseline = np.array(points_baseline)
 
     return points_baseline
+
+
+def improved_baseline():
+    radius = 257 // 2
+
+    cur_x = 0
+    cur_y = 64
+    # points = [(0, i) for i in range(65)]
+    points = [(cur_x, cur_y)]
+
+    for i in range((radius - cur_y) // 2):
+        # go left
+        for i in range(radius - 1):
+            cur_x -= 1
+            points.append((cur_x, cur_y))
+
+        # go up
+        cur_y += 1
+        points.append((cur_x, cur_y))
+
+        # go right
+        for i in range(radius - 1):
+            cur_x += 1
+            points.append((cur_x, cur_y))
+
+        # go up
+        cur_y += 1
+        points.append((cur_x, cur_y))
+
+    # go to (-128, 128)
+    for i in range(radius):
+        cur_x -= 1
+        points.append((cur_x, cur_y))
+
+    # go to (-128, 65)
+    for i in range(radius // 2 + 1):
+        cur_y -= 1
+        points.append((cur_x, cur_y))
+
+    # go to (-128, -127)
+    for i in range((radius - cur_y) // 2 * 3 - 1):
+        # go right
+        for i in range(radius - 1):
+            cur_x += 1
+            points.append((cur_x, cur_y))
+
+        # go down
+        cur_y -= 1
+        points.append((cur_x, cur_y))
+
+        # go left
+        for i in range(radius - 1):
+            cur_x -= 1
+            points.append((cur_x, cur_y))
+
+        # go up
+        cur_y -= 1
+        points.append((cur_x, cur_y))
+
+    # snake to (-128, -1)
+    for i in range(radius // 2 - 1):
+        cur_y -= 1
+        points.append((cur_x, cur_y))
+        cur_x += 1
+        points.append((cur_x, cur_y))
+        cur_y += 1
+        points.append((cur_x, cur_y))
+        cur_x += 1
+        points.append((cur_x, cur_y))
+
+    cur_y -= 1
+    points.append((cur_x, cur_y))
+    cur_x += 1
+    points.append((cur_x, cur_y))
+    cur_y += 1
+    points.append((cur_x, cur_y))
+    cur_y -= 1
+    points.append((cur_x, cur_y))
+
+    # go to (-1, -128)
+    cur_x += 1
+    points.append((cur_x, cur_y))
+
+    # go to (0, 0) (duplicated)
+    for i in range((radius - cur_y) // 4):
+        # go right
+        for i in range(radius):
+            cur_x += 1
+            points.append((cur_x, cur_y))
+
+        # go up
+        cur_y += 1
+        points.append((cur_x, cur_y))
+
+        # go left
+        for i in range(radius):
+            cur_x -= 1
+            points.append((cur_x, cur_y))
+
+        # go up
+        cur_y += 1
+        points.append((cur_x, cur_y))
+
+    # go to (1, 0)
+    cur_x += 1
+    points.append((cur_x, cur_y))
+    # go to (2, 0)
+    cur_x += 1
+    points.append((cur_x, cur_y))
+
+    # go to (1, 126)
+    for i in range((radius - cur_y) // 2 - 1):
+        # go right
+        for i in range(radius - 2):
+            cur_x += 1
+            points.append((cur_x, cur_y))
+
+        # go up
+        cur_y += 1
+        points.append((cur_x, cur_y))
+
+        # go left
+        for i in range(radius - 2):
+            cur_x -= 1
+            points.append((cur_x, cur_y))
+
+        # go up
+        cur_y += 1
+        points.append((cur_x, cur_y))
+
+    # go to (128, 127)
+    # go right
+    for i in range(radius - 2):
+        cur_x += 1
+        points.append((cur_x, cur_y))
+
+    # go up
+    cur_y += 1
+    points.append((cur_x, cur_y))
+
+    # snake to (2, 127)
+    for i in range(radius // 2 - 1):
+        cur_y += 1
+        points.append((cur_x, cur_y))
+        cur_x -= 1
+        points.append((cur_x, cur_y))
+        cur_y -= 1
+        points.append((cur_x, cur_y))
+        cur_x -= 1
+        points.append((cur_x, cur_y))
+
+    # snake to (1, 127)
+    cur_y += 1
+    points.append((cur_x, cur_y))
+    cur_x -= 1
+    points.append((cur_x, cur_y))
+
+    # go to (1, 0)
+    # go right
+    for i in range(radius):
+        cur_y -= 1
+        points.append((cur_x, cur_y))
+
+    return np.array(points)
 
 
 @njit
@@ -140,17 +302,18 @@ def calc_threshold(improve, t_start, t_final, current_itr, max_itr):
     return math.exp(improve / t)
 
 
+@njit
 def two_opt(points, offset, image_lut, t_start, t_end, itr, max_itr):
     i = random.randint(2, len(points) - (4 + offset))
     j = i + 1 + offset
 
-    assert i != 0 and j != len(points) - 1
-
     # A: i-1, B: i, C: j-1, D: j
+    ac_idx = np.array([i - 1, j - 1])
+    bd_idx = np.array([i, j])
     d_AB = evaluate_points(points[i - 1 : i + 1], image_lut)
     d_CD = evaluate_points(points[j - 1 : j + 1], image_lut)
-    d_AC = evaluate_points(points[[i - 1, j - 1]], image_lut)
-    d_BD = evaluate_points(points[[i, j]], image_lut)
+    d_AC = evaluate_points(points[ac_idx], image_lut)
+    d_BD = evaluate_points(points[bd_idx], image_lut)
 
     d0 = d_AB + d_CD
     d1 = d_AC + d_BD
@@ -176,14 +339,23 @@ def two_opt(points, offset, image_lut, t_start, t_end, itr, max_itr):
 def fill_gap_config(config):
     new_config = np.array(config[[0]])
     for i in tqdm(range(1, len(config))):
-        tmp_config = get_path_to_configuration(config[i - 1], config[i])[1:]
-        new_config = np.concatenate([new_config, tmp_config])
+        if np.sum(np.abs(np.array(config[i - 1]) - np.array(config[i]))) != 1:
+            tmp_config = get_path_to_configuration(config[i - 1], config[i])[1:]
+            new_config = np.concatenate([new_config, tmp_config])
+        else:
+            new_config = np.concatenate([new_config, config[[i]]])
     return new_config
 
 
 def local_search(image_lut, max_itr=10, t_start=0.3, t_end=0.001):
 
-    initial_points = get_baseline()
+    start_configs = [
+        [(64, i), (-32, 0), (-16, 0), (-8, 0), (-4, 0), (-2, 0), (-1, 0), (-1, 0)]
+        for i in range(64)
+    ]
+    start_points = [(0, i) for i in range(64)]
+
+    initial_points = improved_baseline()
     initial_score = evaluate_points(initial_points, image_lut)
     print("initial color cost is ", initial_score)
 
@@ -193,7 +365,6 @@ def local_search(image_lut, max_itr=10, t_start=0.3, t_end=0.001):
     best_score = initial_score
     tolerance_cnt = 0
 
-    """
     for itr in tqdm(range(max_itr)):
 
         offset = random.randint(0, 30)
@@ -207,18 +378,19 @@ def local_search(image_lut, max_itr=10, t_start=0.3, t_end=0.001):
             tolerance_cnt += 1
 
         if improve_score < 0:
-            print(improve_score)
             current_score = current_score + improve_score
             points = points_new
 
         if current_score < best_score:
             best_points = points.copy()
             best_score = current_score
-    """
+
+    print(len(set([(p[0], p[1]) for p in best_points.tolist()] + start_points)))
 
     final_score = evaluate_points(best_points, image_lut)
-    # best_config = points_to_path(best_points)
-    best_config = np.array([standard_config(p[0], p[1]) for p in best_points])
+    best_config = np.array(
+        start_configs + [standard_config(p[0], p[1]) for p in best_points]
+    )
     best_config = fill_gap_config(best_config)
     back_config = get_path_to_configuration(np.array(best_config[-1]), get_origin(257))[
         1:
@@ -227,6 +399,8 @@ def local_search(image_lut, max_itr=10, t_start=0.3, t_end=0.001):
         :-1
     ]
     best_config = np.concatenate([start_config, best_config, back_config])
+    best_config = run_remove(best_config)
+    # best_config = run_remove(best_config)
 
     print("improved score is ", final_score)
     return best_config, best_points, initial_score > final_score
